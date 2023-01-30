7bafdd613fd4ef04030afbee58dfd99d7c9dd62e
vaibhav-OP <vaibhavyash28@gmail.com>
2023-01-30 09:33:49 +0530

    Fixed Typo

---
+++ components/Contact/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./component.module.css"
+
+export default function() {
+    return (
+        <section className={styles.contact_wrapper}>
+            <h3 className={styles.headline}>Contact me</h3>
+            <div>
+                <div>Github</div>
+            </div>
+        </section>
+    )
+}
