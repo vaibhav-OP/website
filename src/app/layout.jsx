03caf7a59e61a914e0c3ac70722763d4c3b02e50
vaibhav-OP <vaibhavyash28@gmail.com>
2024-04-24 14:34:38 +0530

    Updated meta tags.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,6 +6,12 @@ const inter = Inter({ subsets: ["latin"] });
 import Navbar from "@/src/components/Navbar";
 import Footer from "@/src/components/Footer";
 
+export const metadata = {
+  title: "Self taught Full-stack web developer",
+  description:
+    "Hi there, my name is Vaibhav Dwivedi, I am from India and I am a self taught web developer.",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
